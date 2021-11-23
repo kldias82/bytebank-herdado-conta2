bdb28a4c96bf3f73d87d67bf669128603732222a
Kleber Dias <kldias@gmail.com>
2021-11-23 09:42:32 -0300

    Upload code

---
+++ bytebank-herdado-conta2/src/Conta.java
@@ -0,0 +1,79 @@
+public class Conta {
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total;
+	
+	public Conta(int agencia, int numero) {
+		Conta.total++;
+		System.out.println("O total de contas é: " + total);
+		this.agencia = agencia;
+		this.numero = numero;
+	}
+	
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saca(valor);
+			destino.deposita(valor);
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+	
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(int Numero) {
+		if(Numero <= 0) {
+			//System.out.println("Numero deve ser maior que zero");
+			return;
+		}
+		this.numero = Numero;
+	}
+
+	public int getAgencia() {
+		return this.agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		if(agencia <= 0) {
+			//System.out.println("Agência deve ser maior que zero");;
+			return;
+		}
+		this.agencia = agencia;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public Cliente getTitular() {
+		return titular;
+	}
+	
+	public static int getTotal() {
+		return Conta.total;
+	}
+}
+	
